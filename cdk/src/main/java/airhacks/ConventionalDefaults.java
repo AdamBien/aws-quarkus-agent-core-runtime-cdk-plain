d6b3b347e12635373545029b142200090ae323b0
Adam Bien <abien@adam-bien.com>
2025-11-02 06:44:18 +0100

    migrated to interface / path to docker added

--- cdk/src/main/java/airhacks/ConventionalDefaults.java
+++ cdk/src/main/java/airhacks/ConventionalDefaults.java
@@ -1,6 +1,11 @@
 package airhacks;
 
-public class ConventionalDefaults {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public interface ConventionalDefaults {
+
+    Path dockerFilePath = Paths.get("..", "agent","src/main/docker/", "Dockerfile").toAbsolutePath();
 
     static String stackName(String appName, String stackName) {
         return "%s-%s-stack".formatted(appName, stackName);
