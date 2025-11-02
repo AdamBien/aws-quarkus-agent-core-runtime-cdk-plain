af97a994afeeb7b2b0653acee195a4b8cf7ead7b
Adam Bien <abien@adam-bien.com>
2025-11-02 08:59:09 +0100

    path fixed

--- cdk/src/main/java/airhacks/ConventionalDefaults.java
+++ cdk/src/main/java/airhacks/ConventionalDefaults.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public interface ConventionalDefaults {
 
-    Path dockerFilePath = Paths.get("..", "agent","src/main/docker/", "Dockerfile").toAbsolutePath();
+    Path dockerFilePath = Paths.get("..", "agent","src/main/docker/").toAbsolutePath();
 
     static String stackName(String appName, String stackName) {
         return "%s-%s-stack".formatted(appName, stackName);
