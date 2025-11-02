03269423802dbc2bd855dd7f32a51a8361d83760
Adam Bien <abien@adam-bien.com>
2025-11-02 11:24:48 +0100

    dockerfile relocated

--- cdk/src/main/java/airhacks/ConventionalDefaults.java
+++ cdk/src/main/java/airhacks/ConventionalDefaults.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public interface ConventionalDefaults {
 
-    Path dockerFilePath = Paths.get("..", "agent","src/main/docker/").toAbsolutePath();
+    Path dockerFilePath = Paths.get("..", "agent").toAbsolutePath();
 
     static String stackName(String appName, String stackName) {
         return "%s-%s-stack".formatted(appName, stackName);
