8ae1ec5b4e9437520500b4b20b6705bb9ef0f302
Adam Bien <abien@adam-bien.com>
2025-11-02 07:16:45 +0100

    ping endpoint implemented

--- agent/src/main/java/airhacks/GreetingAgent.java
+++ agent/src/main/java/airhacks/GreetingAgent.java
@@ -1,8 +1,10 @@
 package airhacks;
 
+import java.time.Instant;
 import java.util.Map;
 
 import jakarta.json.Json;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -28,6 +30,21 @@ public class GreetingAgent {
         var payload = Json.createObjectBuilder()
                 .add("response", message)
                 .add("status", "success")
+                .build();
+        return Response.ok(payload).build();
+    }
+
+    /**
+     * https://docs.aws.amazon.com/bedrock-agentcore/latest/devguide/runtime-http-protocol-contract.html#path-requirements-http
+     * @return
+     */
+    @GET
+    @Path("/ping")
+    public Response ping() {
+        var status = "Healthy";
+        var payload = Json.createObjectBuilder()
+                .add("status", status)
+                .add("time_of_last_update", Instant.now().getEpochSecond())
                 .build();
         return Response.ok(payload).build();
     }
